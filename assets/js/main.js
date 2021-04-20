257394d712709585409fac469e2fb07a7f00bef1
AjayNaruka <naruka.ajay1997@gmail.com>
2021-04-20 14:26:00 +0200

    FINE ( NO BONUS )

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,57 @@
 $(function(){
 
   var nextIcon = $(".carousel i.next")
+  var prevIcon = $(".carousel i.prev")
+
   nextIcon.click( nextPic)
+  prevIcon.click( prevPic)
+
   
   function nextPic(){
 
     // CERCO SLIDE ACTIVE
     var activeSlide = $(".carousel img.active");
-    console.log(activeSlide);
+    var activeCircle = $(".circle.active");
+
     if(activeSlide.hasClass("last")){
       activeSlide.removeClass("active")
+      activeCircle.removeClass("active")
+
       $("img.first").addClass("active")
+      $(".circle.first").addClass("active")
     }else{
       //RIMUOVO ACTIVE
     activeSlide.removeClass("active")
+    activeCircle.removeClass("active")
     //DO ACTIVE AL NEXT
     activeSlide.next().addClass("active")
+    activeCircle.next().addClass("active")
+    
     }
-
     
   }
 
-
-
   function prevPic(){
+
+    // CERCO SLIDE ACTIVE
+    var activeSlide = $(".carousel img.active");
+    var activeCircle = $(".circle.active");
+
+    if(activeSlide.hasClass("first")){
+      activeSlide.removeClass("active")
+      activeCircle.removeClass("active")
+
+      $("img.last").addClass("active")
+      $(".circle.last").addClass("active")
+    }else{
+      //RIMUOVO ACTIVE
+    activeSlide.removeClass("active")
+    activeCircle.removeClass("active")
+    //DO ACTIVE AL NEXT
+    activeSlide.prev().addClass("active")
+    activeCircle.prev().addClass("active")
+    }
+
   }
 
 
